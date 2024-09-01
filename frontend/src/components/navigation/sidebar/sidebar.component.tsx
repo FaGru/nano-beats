527c2408c40271750bb52f3179af060d8bc599f6
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-09-01 19:57:33 +0200

    added layout
    added zustand and updated navbar

--- frontend/src/components/navigation/sidebar/sidebar.component.tsx
+++ frontend/src/components/navigation/sidebar/sidebar.component.tsx
@@ -1,9 +1,31 @@
+'use client';
+import Image from 'next/image';
+import { SidebarList } from './sidebar-list';
+import { navigationConfig } from './sidebar.config';
+import Link from 'next/link';
+import { SidebarOpenClose } from './sidebar-open-close';
+import { useGlobalStore } from '@/lib/state-managment/useGlobalStore';
+
 interface SidebarProps {}
 
 export const Sidebar: React.FC<SidebarProps> = ({}) => {
+  const isSidebarOpen = useGlobalStore((state) => state.isSidebarOpen);
   return (
-    <div className='flex bg-gray-900 border-gray-900 border-r-2 w-44'>
-      <div>sidebar</div>
+    <div
+      className={`relative flex gap-16 flex-col bg-gray-900 ${isSidebarOpen ? 'w-48' : 'w-16'} transition-all duration-500 ease-in-out`}
+    >
+      <div className='p-2 justify-self-center self-center h-16'>
+        <Link href='/'>
+          <Image alt='nano beats' src='./assets/icons/navbar/logo.svg' width={80} height={80} />
+        </Link>
+      </div>
+
+      {navigationConfig.map((config) => (
+        <div key={config.name}>
+          {config.type === 'header' ? <SidebarList config={config} /> : null}
+        </div>
+      ))}
+      <SidebarOpenClose />
     </div>
   );
 };
