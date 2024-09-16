9b0e1bcd43bad72ef96bdccf66efae527fe5450a
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-09-16 21:05:16 +0200

    added drum machine as start page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,9 @@
+import { DrumMachine } from '@/components/drum-machine/drum-machine.comonent';
+
 export default function Home() {
   return (
-    <div className='flex  flex-col items-center justify-between p-24'>
-      Home aadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf
-      adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa
-      dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a
-      sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf
-      adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df
-      adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads
-      fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a
-      dsf adsf a df adsf a sdf a df ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df
-      ads fadsfasdfaadf adsfa dsfadsf a dsf adsf a df adsf a sdf a df ads fadsfasdf
+    <div className='flex flex-col items-center justify-between'>
+      <DrumMachine />
     </div>
   );
 }
