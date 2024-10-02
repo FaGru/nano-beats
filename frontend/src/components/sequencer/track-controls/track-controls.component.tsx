93e59e4816795459d4c40b70901c0fb576340142
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-10-02 19:33:25 +0200

    added scroll area to track controls

--- frontend/src/components/sequencer/track-controls/track-controls.component.tsx
+++ frontend/src/components/sequencer/track-controls/track-controls.component.tsx
@@ -4,6 +4,7 @@ import { TrackEQ } from './track-eq';
 import { TrackDropZone } from './track-dropzone';
 import { TrackDelay } from './track-delay';
 import { TrackPitch } from './track-pitch';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 
 interface TrackControlsProps {
   selectedTrack: TTrack;
@@ -11,19 +12,22 @@ interface TrackControlsProps {
 
 export const TrackControls: React.FC<TrackControlsProps> = ({ selectedTrack }) => {
   return (
-    <div className='flex gap-2 bg-background border-neutral-600 border border-t-0 px-4 rounded-b-md h-[15vh]  p-1.5'>
-      {selectedTrack?.player ? (
-        <>
-          <TrackSample selectedTrack={selectedTrack} />
-          <TrackPitch selectedTrack={selectedTrack} />
-          <TrackEQ selectedTrack={selectedTrack} />
-          <TrackDelay selectedTrack={selectedTrack} />
-        </>
-      ) : (
-        <>
-          <TrackDropZone selectedTrack={selectedTrack} />
-        </>
-      )}
-    </div>
+    <ScrollArea className='flex whitespace-nowrap '>
+      <div className='flex  gap-2 bg-background border-neutral-600 border border-t-0 px-4 rounded-b-md h-[15vh]  p-1.5'>
+        {selectedTrack?.player ? (
+          <>
+            <TrackSample selectedTrack={selectedTrack} />
+            <TrackPitch selectedTrack={selectedTrack} />
+            <TrackEQ selectedTrack={selectedTrack} />
+            <TrackDelay selectedTrack={selectedTrack} />
+          </>
+        ) : (
+          <>
+            <TrackDropZone selectedTrack={selectedTrack} />
+          </>
+        )}
+      </div>
+      <ScrollBar orientation='horizontal' />
+    </ScrollArea>
   );
 };
