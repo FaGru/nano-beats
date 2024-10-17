57eae8c1b26e30d8445f58952cfc38169d76bfda
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-10-17 18:13:39 +0200

    test

--- backend/server.ts
+++ backend/server.ts
@@ -11,22 +11,17 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "https://nano-beats-server.vercel.app"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(function (_, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use("/api/user", userRoutes);
 
