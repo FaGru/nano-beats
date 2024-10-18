4a3cb4a8ea5aa8fa33ab9955f44a3ca7f9e50e24
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-10-18 13:23:37 +0200

    test cors

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -3,19 +3,12 @@ dotenv.config();
 import express from "express";
 import userRoutes from "./user/user.routes";
 import connectDB from "./config/db";
-import "colors";
 import cors from "cors";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-const corsOptions = {
-  origin: ["https://nano-beats.vercel.app", "http://localhost:8000"],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
