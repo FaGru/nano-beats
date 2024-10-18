bfcdeeca4fd5f40f9948ccbd280aaf8e17284505
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-10-18 12:26:02 +0200

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,9 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
