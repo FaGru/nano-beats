10b2ccb9b25835b17f94a3109cbcc9cc414c50c8
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-10-18 12:42:45 +0200

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,21 +9,21 @@ import cors from "cors";
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+const corsOptions = {
+  origin: ["https://nano-beats.vercel.app", "http://localhost:8000"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
 
 connectDB();
+
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-app.use("/api/user", userRoutes);
