e0fd401f91303b3ef49661d19e7bdeb90cc85db4
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-10-18 13:37:07 +0200

    test cors

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -8,7 +8,12 @@ import cors from "cors";
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
