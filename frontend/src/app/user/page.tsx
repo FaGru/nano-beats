18e3db8aff02ce3db3c30e71aa702d1ea6601b39
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-10-19 16:29:00 +0200

    Reapply "added prefetching user"
    This reverts commit b9b5370093f6e0297358a5ca5b5b3b229c34d776.

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,6 +1,9 @@
 import { UserComponent } from '@/components/user/user.component';
 
-export default function UserPage() {
+import { useBasicSSRPrefetchQueries } from '@/lib/hooks/queries/use-basic-ssr-prefetch.queries';
+
+export default async function UserPage() {
+  await useBasicSSRPrefetchQueries();
   return (
     <div className='flex flex-col items-center justify-between'>
       <UserComponent />
