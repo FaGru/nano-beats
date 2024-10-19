486b68bd550892bd3b15bd3cf318f2a3751a1695
Fabian Gruber <95469432+FaGru@users.noreply.github.com>
2024-10-19 12:10:09 +0200

    updated origin

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["https://nano-beats.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
